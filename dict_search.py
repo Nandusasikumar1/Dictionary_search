086f6d0a0e9ed82393d4f67e3e93c796110808e4
Nandu  s <74356710+Nandusasikumar1@users.noreply.github.com>
2024-11-12 18:42:50 +0530

    Update dict_search.py

--- dict_search.py
+++ dict_search.py
@@ -1,22 +1,19 @@
 c = {1:{4:5,7:{8:9}}}
-def search(d,key):
-    temp = {}
-    endcheck = []
+def search1(d,key):
     while True:
+        temp = {}
+        true_count = 0
         if key in d:
             return {key:d[key]}
-        for k,v in d.items():
+        for v in d.values():
             if isinstance(v,dict):
                 temp |= v
-                endcheck.append(False)
-            else:
-                endcheck.append(True)
-        if temp:
-            d,temp = temp,{}
-        if all(endcheck) and key not in d:
+                true_count -= 1
+            true_count += 1
+        d = temp
+        if len(d) == true_count and key not in d:
             return 'key not found'
 
-        endcheck = []
 
 print(search(c,7))
 
